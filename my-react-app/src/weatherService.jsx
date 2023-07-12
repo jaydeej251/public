8f56febf1b88dc500db8174b6671940eb7ecfcfd
Mark Dj Junio <markdjjunio08@gmail.com>
2023-07-12 16:18:28 +0800

    Feat: added Project File
    -working backend
    -working frontend
    - jwt,cors eligible
    -functionalities working

---
+++ my-react-app/src/weatherService.jsx
@@ -0,0 +1,29 @@
+const API_KEY = 'a1c668383abc967adf1163697201018d'
+
+const makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`
+
+const getFormattedWeatherData = async (city, units = 'metric') => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+    const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+    const {weather, main: {temp, feels_like, temp_min, temp_max, pressure, humidity}, wind: { speed },sys: { country }, name, } = data;  
+
+    const {description, icon} = weather[0];
+    return {
+        description, 
+        iconURL: makeIconURL(icon), 
+        temp, 
+        feels_like, 
+        temp_min, 
+        temp_max, 
+        pressure, 
+        humidity, 
+        country,
+        name,
+    };  
+}
+
+export { getFormattedWeatherData };
